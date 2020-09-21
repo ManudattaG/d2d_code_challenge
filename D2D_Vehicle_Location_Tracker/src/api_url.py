9a6aad983b3cad53eaad5e06aa3483ab97adae97
Manudatta G <manudatta4u@gmail.com>
2020-09-21 20:06:00 +0530

    Create api_url.py

---
+++ D2D_Vehicle_Location_Tracker/src/api_url.py
@@ -0,0 +1,3 @@
+registerVehicle_API = "https://vpoe02xgyh.execute-api.us-east-1.amazonaws.com/dev/vehicles"
+deRegisterVehicle_API = "https://245um5jz9c.execute-api.us-east-1.amazonaws.com/dev/vehicles"
+updateVehicleLocation_API = "https://hgge3eylqg.execute-api.us-east-1.amazonaws.com/dev/vehicles"
